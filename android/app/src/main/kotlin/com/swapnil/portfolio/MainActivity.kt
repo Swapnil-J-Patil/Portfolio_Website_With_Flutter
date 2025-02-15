a74769c36e7d1c10245a4fa9e1d3ca2299ae9bd0
Swapnil_Patil <swapnilp0501@gmail.com>
2025-02-15 15:21:50 +0530

    First commit

---
+++ android/app/src/main/kotlin/com/swapnil/portfolio/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.swapnil.portfolio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screens/main/components/coding.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio/components/animated_progress_indicator.dart';
+import '../../../constants.dart';
+
+class Coding extends StatelessWidget {
+  const Coding({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Divider(),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: defaultPadding),
+          child: Text(
+            "Coding",
+            style: Theme.of(context).textTheme.titleMedium,
+          ),
+        ),
+        AnimatedLinearProgressIndicator(
+          percentage: 0.85,
+          label: "kotlin",
+        ),
+        AnimatedLinearProgressIndicator(
+          percentage: 0.7,
+          label: "Dart",
+        ),
+        AnimatedLinearProgressIndicator(
+          percentage: 0.8,
+          label: "Java",
+        ),
+        AnimatedLinearProgressIndicator(
+          percentage: 0.75,
+          label: "Retrofit",
+        ),
+        AnimatedLinearProgressIndicator(
+          percentage: 0.70,
+          label: "Room Database",
+        ),
+        AnimatedLinearProgressIndicator(
+          percentage: 0.85,
+          label: "Firebase",
+        ),
+      ],
+    );
+  }
+}
